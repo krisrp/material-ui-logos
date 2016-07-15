0c07b8d74f0a83fb62960d78ee708609405c16d2
Kris Pritchard <krisrp@users.noreply.github.com>
2016-07-15 12:50:59 -0700

    Add simple logo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # material-ui-logos
 SVG logos for use with material-ui
+
+Depends on material-ui
+
+    npm install --save material-ui-logos
+
+    import LogoName from 'material-ui-logos'
---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ActionAccountBalance = undefined;
+
+var _accountBalance = require('./logos/account-balance');
+
+var _accountBalance2 = _interopRequireDefault(_accountBalance);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.ActionAccountBalance = _accountBalance2.default;
